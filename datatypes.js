30d765520e286147f953f2112240c01c5eb6d528
Ajith PM <ajithpmdwaraka@gmail.com>
2024-12-26 22:12:33 +0530

    DataTypes Added

---
+++ datatypes.js
@@ -0,0 +1,19 @@
+//Primitive DataTyoes
+let a = 20; //Numbers
+let b = "Hello JS"; //String
+let isLogged = true; //Boolean
+let value; //undefined
+let empty = null; //null
+let sym = Symbol('unique'); //symbol
+let big = 123456789012345678901234567890n; //BigInt
+
+//Non Primitive DataTypes
+const student = { name: "Luna", age: 30 }; //Object 
+let arr = [1, 2, 3, 4]; //Array
+function car() {
+    return "This is a car";
+  } //Function
+  
+
+
+
