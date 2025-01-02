9e64c639e3548f02a358a29898b99a5e31b69db5
Ajith PM <ajithpmdwaraka@gmail.com>
2025-01-02 22:14:36 +0530

    else-if Added

---
+++ else-if-statement.js
@@ -0,0 +1,8 @@
+let age = 20;
+if(age> 18){
+    console.log("You are eligible to vote");
+} else if(age>18){
+    console.log("You are ineligible to vote")
+}else{
+    console.log("Wait to grow");
+}
