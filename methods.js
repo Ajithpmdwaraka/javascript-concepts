38d64a08ce604e2d3ea255165ebec59de7fbb84d
Ajith PM <ajithpmdwaraka@gmail.com>
2025-01-06 22:08:17 +0530

    Methods in Javascript

---
+++ methods.js
@@ -0,0 +1,11 @@
+const calculator = {
+    add: function(a, b) {
+        return a + b;
+    },
+    subtract: function(a, b) {
+        return a - b;
+    }
+};
+
+console.log(calculator.add(5, 10)); // Output: 15
+console.log(calculator.subtract(10, 5)); // Output: 5
